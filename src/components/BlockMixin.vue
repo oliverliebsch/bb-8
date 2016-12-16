50807389f5de1bed58d0a8e0a91a01af4cd646c3
Oliver Liebsch <liebsch@gamboo.net>
2016-12-16 11:46:19 +0100

    Add config to block mixin

--- src/components/BlockMixin.vue
+++ src/components/BlockMixin.vue
@@ -3,7 +3,7 @@
 export default {
   name: 'block-mixin'
 
-  props: ['block', 'index']
+  props: ['block', 'index', 'config']
 
   created: ->
     this.block.fields = this.fields if Object.keys(this.block.fields).length <= 0
